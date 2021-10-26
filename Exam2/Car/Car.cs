860ed1cf91990b98b1a776742c11efd7c51d4de6
Jennifer Ha <Jennifer.Ha-1@ou.edu>
2021-10-26 00:20:35 -0500

    trying to commit first

---
+++ Exam2/Car/Car.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Car
+{
+    public class Car
+    {
+        public string CarMake {get;set;}
+        public string CarColor { get; set; }
+        public int Year { get; set; }
+        public int Mileage { get; set; }
+        public string Picture { get; set; }
+
+        public override string ToString()
+        {
+            return $"{CarMake}";
+        }
+    }
+}
---
+++ Exam2/Car/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Car
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private List<Car> C = new List<Car>(); 
+        public MainWindow()
+        {
+            InitializeComponent();
+            using (var client= new HttpClient())
+            {
+                string jsonData = client.GetStringAsync("https://timmyluong11.github.io/JamJamTestJSON/RandCar2.json").Result;
+
+
+
+            }
+        }
+    }
+}
