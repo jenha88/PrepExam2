a06b699b6cb5a14ddb13fa1cfc05a5512ab1a9b5
Jennifer Ha <Jennifer.Ha-1@ou.edu>
2021-10-26 01:40:55 -0500

    commitings things

--- Exam2/Car/MainWindow.xaml.cs
+++ Exam2/Car/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Net.Http;
 using System.Text;
@@ -21,17 +23,94 @@ namespace Car
     /// </summary>
     public partial class MainWindow : Window
     {
-        private List<Car> C = new List<Car>(); 
+        private List<Car> C = new List<Car>();
+        private List<Car> c;
         public MainWindow()
         {
             InitializeComponent();
             using (var client= new HttpClient())
             {
                 string jsonData = client.GetStringAsync("https://timmyluong11.github.io/JamJamTestJSON/RandCar2.json").Result;
+                //Car cc = JsonConvert.DeserializeObject<Car>(jsonData);
+               List<Car> beep = JsonConvert.DeserializeObject<List<Car>>(jsonData);
+                foreach (var item in beep)
+                {
+                    C.Add(item);
 
+                }
+             
+            }
+            PopulateCB(C);
+        }
+        private void PopulateCB(List<Car> vroom)
+        {
+            cbColor.Items.Add("All");
+            cbColor.SelectedIndex = 0;
 
+            foreach (var item in vroom)
+            {
+                if (!cbColor.Items.Contains(item.CarColor))
+                {
+                    cbColor.Items.Add(item.CarColor);
+
+                }
+            }
+        }
+        private List<Car>FilterColor(List<Car> F)
+        {
+            List<Car> c = new List<Car>();
+            string f = cbColor.SelectedValue.ToString();
+
+            foreach (var item in F)
+            {
+                if (f.ToLower() == "all")
+                {
+                    c.Add(item);
+                }
+                else if (item.CarColor.Contains(f))
+                {
+                    c.Add(item);
+                }
 
             }
+            return c; 
+        }
+        private List<Car> FilterYear(List<Car> FF)
+        {
+            List<Car> c = new List<Car>(); 
+            int YR = 0;
+            string Y = txtYear.Text;
+            if (string.IsNullOrEmpty(Y) == true)
+            {
+                MessageBox.Show("╚(•⌂•)╝ I can't read this stupid!");
+            }
+            else if (int.TryParse(Y, out YR) == false)
+            {
+                MessageBox.Show("┻━┻ ︵ヽ(`Д´)ﾉ︵ ┻━┻ this isn't a number!");
+            }
+            foreach (var item in FF)
+            {
+                if (item.Year>=YR)
+                {
+                    c.Add(item);
+                }
+            }
+            return c;
+        }
+        private void btbPush_Click(object sender, RoutedEventArgs e)
+        {
+            c = FilterColor(C);
+            c = FilterYear(C);
+            PopulateCB(c);
+            
+        }
+
+        private void btbExport_Click(object sender, RoutedEventArgs e)
+        {
+            string jj = JsonConvert.SerializeObject(c);
+            File.WriteAllText("car.json", jj);
+
+            MessageBox.Show("(∩^o^)⊃━☆ Yay it successful exported!");
         }
     }
 }
